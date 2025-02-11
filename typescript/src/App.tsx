9cf97c8c73955578bcfa98623a0c32c95ed8ad9c
Bhammond9 <bhammond901@yahoo.com>
2025-02-10 17:24:44 -0700

    Created onClick Functionality

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import { useState } from "react";
+import Modal from "./components/modal";
+
+const handleOpen = setShowModal(true)
+const handleClose = setShowModal(false)
+
 function App() {
   return (
     <div className="App">
+      {showModal && <Modal onClose = {handleClose}/>}
     <h1>My Birthday Gifts</h1>
     <div className="cards">
       <button>Add a Gift</button>
